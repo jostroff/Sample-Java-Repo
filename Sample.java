604981d61b747b59bbf369053c93c2814a23f50a
Ostroff <185874@NTTDATA.COM>
2019-06-18 11:25:32 -0500

    My java files.

---
+++ Sample.java
@@ -0,0 +1,14 @@
+package p1;
+//import java.lang.String;
+//import java.lang.*;
+
+class Sample
+{
+	public static void main(String[] args){
+		System.out.println("Hello world!");
+	}
+
+	int add(int a, int b){
+		return a+b;
+	}
+}
